b61a2312cb579c9868bb3a542baea03726706746
Lance Rushing <lance@lancerushing.com>
2019-08-17 18:30:43 -0700

    Add thread example

---
+++ src/threads.cpp
@@ -0,0 +1,45 @@
+/**
+ * Example of how to start multiple threads.
+ */
+
+#include "vex.h"
+
+// Global Variables
+vex::brain Brain;
+
+void myThread1() {
+    int count = 0;
+  
+    while(true) {
+        Brain.Screen.printAt(10, 40, "myThread1 %d", count++);
+      
+        vex::this_thread::sleep_for(100); // Let other threads run
+    }
+}
+
+void myThread2() {
+    int count = 0;
+   
+    while(true) {
+        Brain.Screen.printAt(10, 60, "myThread2 %d", count++);
+   
+        vex::this_thread::sleep_for(200); // Let other threads run
+    }
+}
+
+int main() {
+    // start the threads
+    vex::thread t1( myThread1 );
+    vex::thread t2( myThread2 ); 
+
+    Brain.Screen.printAt(0, 110, "Theads have Started.");
+    
+    // Wait, then stop the threads 
+    vex::this_thread::sleep_for(10000); // 10 seconds
+    t1.interrupt();
+    t2.interrupt();
+
+    Brain.Screen.printAt(0, 130, "All Done");
+
+    // Note: The program will only end if all threads are stopped
+}
